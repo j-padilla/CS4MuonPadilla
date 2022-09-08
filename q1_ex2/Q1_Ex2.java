3942fd41bb39047df626d0e3a3e80cd706d6a439
j-padilla <100137777+j-padilla@users.noreply.github.com>
2022-09-08 11:24:41 +0800

    Add files via upload

---
+++ q1_ex2/Q1_Ex2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q1_ex2;
+
+/**
+ *
+ * @author MUON
+ */
+public class Q1_Ex2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Welcome to Higher or Lower! What will you do? \n-Start Game \n-Change Settings \n-End Application");
+    }
+    
+}
