60af105297f3cb3e5bf84509ecf587182dc0cfc6
j-padilla <100137777+j-padilla@users.noreply.github.com>
2022-09-08 11:17:13 +0800

    Update Q1_Ex1.java

--- q1_ex1/Q1_Ex1.java
+++ q1_ex1/Q1_Ex1.java
@@ -20,6 +20,16 @@ public class Q1_Ex1 {
       int jt = 4;
       int ty = mt + jt;
       boolean isMore = true;
+      String mtSubj = "Computer Science";
+      String jtSubj = "Social Science";
+      boolean sS = true;
+      
+      if(mtSubj.equals(jtSubj)) {
+          sS = true;
+      }
+      else {
+          sS = false;
+      }
       
       if(jt > mt) {
          isMore = true;
@@ -28,10 +38,12 @@ public class Q1_Ex1 {
          isMore = false;
       }
             
-      //System.out.printf("Teacher 1 %n Name: %s %n Subject: ");
-      System.out.println("Total years of teaching: " + ty);
+      System.out.printf("Teacher 1 %nName: Mark Tarcelo %nSubject: %s  %nYears of Teaching: %d %n",mtSubj, mt );
+      System.out.printf("%nTeacher 2 %nName: Joshua Tarcelo %nSubject: %s  %nYears of Teaching: %d %n",jtSubj, jt );
+
+      System.out.printf("%nTotal years of teaching: %d %n",ty);
       System.out.println("Joshua Tarcelo has more years of teaching: " + isMore);
-      System.out.println("The teachers have the same subject: " );
+      System.out.printf("The teachers have the same subject: " + sS );
       
     }
     
