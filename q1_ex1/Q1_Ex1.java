c2b6c6c88bce84065e0fc5ecad472a236ff09651
j-padilla <100137777+j-padilla@users.noreply.github.com>
2022-09-15 11:12:48 +0800

    Update Q1_Ex1.java

--- q1_ex1/Q1_Ex1.java
+++ q1_ex1/Q1_Ex1.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package q1_ex1;
 
 /**
@@ -15,7 +11,6 @@ public class Q1_Ex1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
       int mt = 7;
       int jt = 4;
       int ty = mt + jt;
