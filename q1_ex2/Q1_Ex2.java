9602afe4b73f212efa2df129752342ba7a3ebdc9
j-padilla <100137777+j-padilla@users.noreply.github.com>
2022-09-15 11:27:40 +0800

    Update Q1_Ex2.java

--- q1_ex2/Q1_Ex2.java
+++ q1_ex2/Q1_Ex2.java
@@ -16,27 +16,37 @@ public class Q1_Ex2 {
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         int random = (int) Math.floor(Math.random()*10) + 1;
-        
-        String trueStart = "start game";
-        String theSettings = "change settings";
-        String endApplication = "end application";
+        int turns = 3;        
+
+        String start = "";
         int guess = 0;
-        int turns = 3;
-        boolean guesses = true;
-        
-        System.out.print("Welcome to Higher or Lower! What will you do? \n-Start Game \n-Change Settings \n-End Application \n");
-        String start = sc.nextLine();
-        
-        int randNum = sc.nextInt();
-        
-        if(start.equalsIgnoreCase(trueStart)) System.out.printf("%nYou have %d guess(es) left. What is your guess? %d",turns, guess);
-        else if(start.equalsIgnoreCase(theSettings)) System.out.println("\nWhat is the lower limit of number? \nWhat is the higher limit of the number? \nHow many guesses are allowed?");
-        else if(start.equalsIgnoreCase(endApplication)) System.out.println("Thank you for playing!");
-        
-        while(turns != 0)
-        
-        if(randNum == (random)) System.out.println("\nYou got it! \nPlay Again (y/n)? ");        
+        int lLim = 5;
+        int hLim = 10;
+                
+        do {
+          System.out.print("Welcome to Higher or Lower! What will you do?\n" 
+                  + "-Start game\n" 
+                  + "-Change settings\n" 
+                  + "-End application\n");
+          start = sc.nextLine();
+          
+          if(start.equalsIgnoreCase("start game")) {
+              do {
+                System.out.printf("You have %d guess(es) left. What is your guess? ", turns);
+                guess = sc.nextInt();
+              }while(turns != 0);
+          }
+          else if(start.equalsIgnoreCase("change settings")) {
+              System.out.println("What is the lower limit of the random number?");
+              lLim = sc.nextInt();
+              System.out.println("What is the upper limit of the random number?");
+              hLim = sc.nextInt();
+              System.out.println("How many guesses are allowed?");
+              turns = sc.nextInt();
+          }
+        }
+        while(!start.equalsIgnoreCase("end application")); 
     }
-    
 }
