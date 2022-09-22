c42a3c916f9bc369232fc67a4509271b3c43dc41
j-padilla <100137777+j-padilla@users.noreply.github.com>
2022-09-22 11:25:33 +0800

    Update Singer.java

--- q1_ex3/Singer.java
+++ q1_ex3/Singer.java
@@ -9,5 +9,28 @@ package q1_ex3;
  * @author babyj
  */
 public class Singer {
+    String name, favoriteSong;
+    int noOfPerformances, audience;
+    double earnings;
+    boolean performForAudience;
     
+    public Singer(String n) {
+        this.name = n;
+        favoriteSong = "";
+        noOfPerformances = 0;
+        earnings = 0;
+        performForAudience = false;
+        audience = 0;
+    }
+    
+    public void performForAudience() {
+        if(performForAudience == true) {
+            noOfPerformances =+ 1;
+            earnings = audience * 100;
+        }
+    }
+    
+    public void changeFavSong(String n) {
+        this.favoriteSong = n;
+    }
 }
