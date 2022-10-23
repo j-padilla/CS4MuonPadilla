1cacbff8f026bb451a6cf54f21880daa9264c6bd
j-padilla <100137777+j-padilla@users.noreply.github.com>
2022-10-24 01:54:59 +0800

    Update Song.java

--- q1_ex3/Song.java
+++ q1_ex3/Song.java
@@ -9,15 +9,52 @@ package q1_ex3;
  * @author babyj
  */
 public class Song {
-    String title, genre, artist;
-    int popularityStreams;
-    double songTime;
+    private String title, album;
+    private int songNo, popularityFaved, timesPlayed;
     
-    public Song(String n) {
-        this.title = n;
-        this.genre = n;
-        this.artist = n;
-        popularityStreams = 0;
-        songTime = 0;
+    public Song(String title) {
+        this.title = title;
+        this.timesPlayed = 0;
+        this.popularityFaved = 0;
+    }
+    
+    public Song(String title, String album, int songNo) {
+        this.title = title;
+        this.album = album;
+        this.songNo = songNo;
+        this.timesPlayed = 0;
+        this.popularityFaved = 0;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public int getSongNo() {
+        return songNo;
+    }
+
+    public int getPopularityFaved() {
+        return popularityFaved;
+    }
+
+    public int getTimesPlayed() {
+        return timesPlayed;
+    }
+    
+    public void setSongNo(int songNo) {
+        this.songNo = songNo;
+    }   
+    
+    public void play() {
+        timesPlayed ++;
+    }
+    
+    public void favorite() {
+        popularityFaved ++;
     }
 }
