16e3c804269faee4af46b0a58d40cc86d19db230
j-padilla <100137777+j-padilla@users.noreply.github.com>
2022-10-24 02:12:43 +0800

    Update Q1_Ex3.java

--- q1_ex3/Q1_Ex3.java
+++ q1_ex3/Q1_Ex3.java
@@ -23,9 +23,18 @@ public class Q1_Ex3 {
         Singer ra = new Singer("Rude-a");
         Singer ts = new Singer("Troye Sivan", s2);
         
+        Album p1 = new Album("KUMI", 43);
+        Album p2 = new Album("Ten2", 12);
+        Album p3 = new Album("Rnds", 24);
+        
         ra.performForAudience(12);
         ra.setFavoriteSong(s4);
         
+        p2.setTopPlayed(s2);
+        p1.setNoOfSongs(43);
+                
         System.out.println(ra.getFavoriteSong().getTitle());
+        System.out.println(p2.getTopPlayed().getTitle());
+        System.out.println(p1.getNoOfSongs());
     }
 }
