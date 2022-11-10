8b721b7bfd1e45a0841e6e61c3425ea3ab8cb431
j-padilla <100137777+j-padilla@users.noreply.github.com>
2022-11-10 10:59:02 +0800

    Update WaterType.java

--- q2_ex6/WaterType.java
+++ q2_ex6/WaterType.java
@@ -9,5 +9,7 @@ package q2_ex6;
  * @author babyj
  */
 public class WaterType {
-    
+    public WaterType(String n, String t, String s, String w, int m, int base) {
+        super(n, "water", "fire", "grass", m, base);
+    }
 }
