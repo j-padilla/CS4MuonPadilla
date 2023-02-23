087de28a62d629ae5247a8689e48abfae7b60de2
j-padilla <100137777+j-padilla@users.noreply.github.com>
2023-02-23 11:19:59 +0800

    Add files via upload

---
+++ trial00/FXMLDocumentController.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package trial00;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Bree Sison
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
